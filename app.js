7fbee474b9ca8ba8dd3498b9a6875d5f19192e0a
Ofentse-T <122441914+Ofentse-T@users.noreply.github.com>
2023-11-08 15:26:34 +0200

    Added logo

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import bio from './Modules/bio.js'
 import gallery from './Modules/gallery.js'
 import nav from './Modules/nav.js'
 
-const app = () => {
-    return `
-    ${nav()}
-    <div class = "container">
-    ${bio()}
-    ${gallery()}
-    </div>
-    `
-}
+// const app = () => {
+//     return `
+//     ${nav()}
+//     <div class = "container">
+//     ${bio()}
+//     ${gallery()}
+//     </div>
+//     `
+// }
 
 document.getElementById('root').innerHTML = app();
